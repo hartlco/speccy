a5985537bd0a9fab0b77a6066923acfe2fcb9889
Martin Hartl <martin@hartl.co>
2025-08-23 05:58:11 +0200

    Initial Commit

---
+++ speccy/speccyApp.swift
@@ -0,0 +1,17 @@
+//
+//  speccyApp.swift
+//  speccy
+//
+//  Created by Martin Hartl on 23.08.25.
+//
+
+import SwiftUI
+
+@main
+struct speccyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
